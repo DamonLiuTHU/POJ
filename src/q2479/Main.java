099e6a5f2f4445257625c4ae5307ecf40ebab2be
DamonLiu-MacbookPro <liu394000785@sina.com>
2015-07-23 12:37:05 +0800

    q2479 the final answer,logically correct

--- src/q2479/Main.java
+++ src/q2479/Main.java
@@ -17,30 +17,45 @@ public class Main {
 		re_array = new int[input.length];
 		length = input.length;
 		max = 0;
+		if(length == 1){
+			return input[0];
+		}
 		array[0] = input[0];
-		array[length - 1] = getMax(length-1);
-		re_array[length-1] = 0;
-		re_array[0] = getRigthMax(0);
+		re_array[length-1] = input[length-1];
 		for (int i = 1; i < length-1; i++) {
-			array[i] = getMax(i);
+			array[i] = getLeftMax(i);
 			int index = length - 1 - i;
 			re_array[index] = getRigthMax(index);
 		}
+		array[length - 1] = getLeftMax(length-1);
+		re_array[0] = getRigthMax(0);
 		for (int i = 0; i < length - 1; i++) {
-			if (array[i] + re_array[i + 1] > max) {
-				max = array[i] + re_array[i + 1];;
+			int temp =array[i] + getMaxValueFromIndex(i+1); 
+			if ( temp > max) {
+				max = temp;
 			}
 		}
+//		max -= input[anchor];
 //		result = max;
+		return max;
+	}
+	
+	private static int getMaxValueFromIndex(int index){
+		int max = Integer.MIN_VALUE;
+		for(int i = index;i<length;i++){
+			if(re_array[i]>max){
+				max = re_array[i];
+			}
+		}
 		return max;
 	}
 
 	private static int getRigthMax(int index) {
-		index += 1;
-		return Math.max(input[index] + re_array[index], input[index]);
+//		index += 1;
+		return Math.max(input[index] + re_array[index+1], input[index]);
 	}
 
-	private static int getMax(int index) {
+	private static int getLeftMax(int index) {
 		return Math.max(array[index - 1] + input[index], input[index]);
 	}
 
