1ddc2058ce6eefac12644294776ddc604f36e2d0
DamonLiu-MacbookPro <liu394000785@sina.com>
2015-07-30 16:14:26 +0800

    so difficult...I feel so bad about going on

---
+++ src/q2109/Main.java
@@ -0,0 +1,32 @@
+package q2109;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Vector<Integer> result = new Vector<Integer>();
+//		while(true){
+////			String[] numbers_string = temp.split(" ");
+////			double n = Double.parseDouble(numbers_string[0]);
+////			double p = Double.parseDouble(numbers_string[1]);
+//			double n = sc.nextDouble();
+//			double p = sc.nextDouble();
+//			
+//			double k;
+//			k = Math.pow(p, 1.0/n);
+//			int temp1 = (int)k;
+//			result.add(temp1);
+//		}
+//		
+//		for(int temp : result){
+//			System.out.println(temp);
+//		}
+//		
+//		System.out.println((double)Math.pow(1233, 7));
+	}
+
+}
