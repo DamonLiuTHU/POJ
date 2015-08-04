39c1f5ae93ac8287e28a07d0d609f1b8e60c94b3
DamonLiu-MacbookPro <liu394000785@sina.com>
2015-08-04 13:47:49 +0800

    hiho1038
    really a nice example for DP

---
+++ src/hiho1032/Main.java
@@ -0,0 +1,42 @@
+package hiho1032;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 先从标准输入读取一个整数N（N<=30)，代表我给你的字符串的个数，然后接下来的就是我要给你的那N个字符串（字符串长度<=10^6)啦。
+		 * 而你要告诉我你的答案的话，只要将你计算出的最长回文子串的长度按照我给你的顺序依次输出到标准输出就可以了！你看这就是一个例子。”
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		while(N>0){
+			N--;
+			String s = sc.next();
+			System.out.println(getMaxLength(s));
+		}
+	}
+
+	private static int getMaxLength(String string) {
+		int MAX_LENGTH=0;
+		// 找到最长回文串 version01 使用stack  //faile
+//		Stack<Character> stack = new Stack<Character>();
+//		for(int i=0;i<string.length()/2+1;i++){
+//			char tmp = string.charAt(i);
+//			if(stack.isEmpty()){
+//				stack.push(tmp);
+//			}else{
+//				char top = stack.peek();
+//				if(top!=)
+//			}
+//		}
+		
+		
+		return MAX_LENGTH;
+	}
+
+}
--- src/hiho1038/Main.java
+++ src/hiho1038/Main.java
@@ -3,7 +3,9 @@ package hiho1038;
 import java.util.Scanner;
 
 public class Main {
-	
+	/*
+	 * 动态规划   内存的节省。
+	 */
 	class Item{
 		public int need,value;
 		public Item(int need,int value){
@@ -15,19 +17,43 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc =new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int N = sc.nextInt();//奖品的数量
+		int M = sc.nextInt();//小明有的所有钱
 		Main m = new Main();
-		Item[] array = new Item[N];
-		for(int l=0;l<N;l++){
+		Item[] array = new Item[N+1];
+		for(int l=1;l<=N;l++){
 			int need = sc.nextInt();
 			int value = sc.nextInt();
 			array[l] = m.new Item(need,value);
 		}
 		int Ans=-1;
+//		int[][] best = new int[N+1][M+1];
+//		for(int i=1;i<=N;i++){
+//			Item item = array[i];
+//			for(int j=1;j<=M;j++){
+//				if(j<item.need){
+//					best[i][j] = best[i-1][j];
+//				}else{
+//					best[i][j] = Math.max(best[i-1][j-item.need]+item.value, best[i-1][j]);
+//				}
+//			}
+//		}
+		int best[] = new int[M+1];
+		for(int i=1;i<N;i++){
+			Item item = array[i];
+			for(int j=M;j>item.need;j--){
+//				if(j<item.need){
+//					best[j] = 
+//				}else{
+					best[j] = Math.max(best[j],best[j-item.need]+item.value);
+//				}
+			}
+		}
 		
-		
-		System.out.println(Ans);
+		System.out.println(best[M]);
 	}
+	
+	
+	
 
 }
