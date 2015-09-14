7aeb40bc1ab54bbfe2e7af26dc5da4fbf6ffe930
DamonLiuNJU <liu394000785@sina.com>
2015-09-14 15:43:55 +0800

    created files for POJ1050

---
+++ src/q1050/Main.java
@@ -0,0 +1,15 @@
+package q1050;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			
+		}
+	}
+
+}
