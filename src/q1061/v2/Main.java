8c320930489f0418fa8e3831815f51695530a170
DamonLiuNJU <liu394000785@sina.com>
2015-09-15 19:23:19 +0800

    扩展欧几里德算法，毕竟不是自己专业，有难度。。
    只是复习一下GCD的欧几里德算法，GCD（a，b）＝GCD（b，amod b）

--- src/q1061/v2/Main.java
+++ src/q1061/v2/Main.java
@@ -1,45 +1,57 @@
 package q1061.v2;
 
 import java.util.Scanner;
+
 /*
- * 
+ * TL  1000MS
  */
 public class Main {
-	
-	public static long gcd(long a, long b) {  
-	    while(b!=0) {  
-	        long r = a % b;  
-	        a = b;  
-	        b = r;  
-	    }  
-	    return a;  
-	}  
+
+	private static long GCD(long a, long b) {
+		while (b != 0) {
+			long r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
 
 	public static void main(String[] args) {
+		System.out.println(GCD(12,16));
 		Scanner in = new Scanner(System.in);
 		while (in.hasNext()) {
-//			int a = in.nextInt();
-//			int b = in.nextInt();
-//			System.out.println(a + b);
-			long x = in.nextLong();
-			long y = in.nextLong();
-			long m = in.nextLong();
-			long n = in.nextLong();
-			long L = in.nextLong();
-			long diff1 = x-y;
-			long diff2 = m-n;
-			
-			if(m==n&&diff1%L!=0){
+			// int a = in.nextInt();
+			// int b = in.nextInt();
+			// System.out.println(a + b);
+			long x = in.nextLong(); // A's start point
+			long y = in.nextLong(); // B's start point
+			long m = in.nextLong(); // A's one jump length
+			long n = in.nextLong(); // B's one jump length
+			long L = in.nextLong(); // the length of the cycle
+			long diff1 = x - y;
+//			long diff2 = m - n;
+
+			if (m == n && diff1 % L != 0) {
 				System.out.println("Impossible");
-			}
-			for(int i=0;i<Integer.MAX_VALUE;i++){
-				if(	(diff1+diff2*i)%L == 0){
-					System.out.println(i);
-					break;
-				}
+			} else {
+//				for (int i = 0; i < Integer.MAX_VALUE; i++) {
+//					if ((diff1 + diff2 * i) % L == 0) {
+//						System.out.println(i);
+//						break;
+//					}
+//				}
+				System.out.println(getSmallestValue(x,y,m,n,L));
 			}
 		}
 
+	}
+
+	private static int getSmallestValue(long x, long y, long m, long n, long l) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		return 0;
 	}
 
 }
