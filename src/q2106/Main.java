5d55a6bc41d06f6d154b56474f192c4f72455432
DamonLiuNJU <liu394000785@sina.com>
2015-09-16 20:23:43 +0800

    Small fix about the program

--- src/q2106/Main.java
+++ src/q2106/Main.java
@@ -3,22 +3,21 @@ package q2106;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
+
 /**
- * 神一样的测试数据 
- * F|(F)
- * !!!!!F
- * (((V)))  这三个过了就OK了。  好厉害！
+ * 神一样的测试数据 F|(F) !!!!!F (((V))) 这三个过了就OK了。 好厉害！
+ * 
  * @author damonliu
  *
  */
 public class Main {
 	static HashMap<Character, Integer> inStackPriority = new HashMap<Character, Integer>(4);
 	static HashMap<Character, Integer> outStackPriority = new HashMap<Character, Integer>(4);
-	static Stack<Character> operator ;
-	static Stack<Character> VF ;
+	static Stack<Character> operator;
+	static Stack<Character> VF;
+	private static Scanner sc;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		inStackPriority.put(')', 20);
 		outStackPriority.put(')', 1);
 		inStackPriority.put('!', 12);
@@ -31,20 +30,20 @@ public class Main {
 		outStackPriority.put('(', 20);
 		inStackPriority.put('#', 0);
 		outStackPriority.put('#', 0);
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		int counter = 0;
 		while (sc.hasNext()) {
 			String expression = sc.nextLine();
-			if(expression.compareToIgnoreCase("")==0){
+			if (expression.compareToIgnoreCase("") == 0) {
 				continue;
 			}
 			counter++;
 			expression = expression.trim();
-//			operator.clear();
-//			VF.clear();
-			operator =  new Stack<Character>();
+			// operator.clear();
+			// VF.clear();
+			operator = new Stack<Character>();
 			operator.push('#');
-			VF =  new Stack<Character>();
+			VF = new Stack<Character>();
 			for (int i = 0; i < expression.length(); i++) {
 				char tmp = expression.charAt(i);
 				if (tmp == ' ')
@@ -54,7 +53,7 @@ public class Main {
 					int tmpInsidePriority = inStackPriority.get(operator.peek());
 					if (tmpOutPriority > tmpInsidePriority) {
 						operator.push(tmp);
-					}else if(tmpOutPriority==tmpInsidePriority){
+					} else if (tmpOutPriority == tmpInsidePriority) {
 						operator.pop();
 					} else if (tmpOutPriority < tmpInsidePriority) {
 						char op = operator.pop();
@@ -71,7 +70,7 @@ public class Main {
 							operator.pop();
 						} else {
 							op = operator.peek();
-							while(outStackPriority.get(tmp)<inStackPriority.get(op)){
+							while (outStackPriority.get(tmp) < inStackPriority.get(op)) {
 								VF.push(Main.calculate(operator.pop()));
 								op = operator.peek();
 							}
@@ -87,33 +86,37 @@ public class Main {
 				VF.push(Main.calculate(op));
 			}
 			System.out.println("Expression " + counter + ": " + VF.pop());
-			VF = null; operator = null;
+			VF = null;
+			operator = null;
+			System.gc();
 		}
 	}
-
+	static char True = 'V';
+	static char False = 'F';
+	
 	private static char calculate(char op) {
-		// TODO Auto-generated method stub
 		char o1, o2;
 		switch (op) {
 		case '|':
 			o1 = VF.pop();
 			o2 = VF.pop();
-			if (o1 == 'V' || o2 == 'V')
-				return 'V';
+			if (o1 == True || o2 == True)
+				return True;
 			else
-				return 'F';
+				return False;
 		case '&':
 			o1 = VF.pop();
 			o2 = VF.pop();
-			if (o1 == 'V' && o2 == 'V')
-				return 'V';
+			if (o1 == True && o2 == True)
+				return True;
 			else
-				return 'F';
+				return False;
 		case '!':
 			o1 = VF.pop();
-			return o1 == 'V' ? 'F' : 'V';
+			return o1 == True ? False : True;
 		default:
-			return 'N';
+			System.exit(404);
+			return '?';
 		}
 
 	}
